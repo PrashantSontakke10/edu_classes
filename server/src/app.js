378d073180c0ca0d9749c3e1e0b2e480049d40ac
Prashant Sontakke <prashantsontakke5460@gmail.com>
2026-01-15 13:22:05 +0530

    Backend connect with frontend

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,21 @@
 import express from "express";
 import cors from "cors";
-import connectDB from "./config/db.js";
+import testRoutes from "./routes/test.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
+
+
+app.use("/api/test", testRoutes);
 
 export default app;
