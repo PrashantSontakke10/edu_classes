ec7ba3c0f8749e931b558635def59d775d50fecb
ayush <ayushdhewale@gmail.com>
2026-01-15 13:55:08 +0530

    Sample branch

--- server/src/server.js
+++ server/src/server.js
@@ -20,6 +20,7 @@ connectDb().then(
      async () => {
         app.listen(PORT, () => {
           console.log(`Server is running on port ${PORT}`);
+          console.log(`Environment: ${process.env.NODE_ENV}`);
         });
      }
   ).catch((error) => {
